1842793d3cc5c46bd7e85ebad11339e27ae03af2
kaito <kaitooshiro1@icloud.com>
2020-04-14 09:45:23 +0900

    seedの順番変更

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(QuestionTableSeeder::class);
         $this->call(CategoryTable::class);
+        $this->call(QuestionTableSeeder::class);
     }
 }
