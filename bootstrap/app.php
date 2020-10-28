330965cc621c302a5f2be7759e53dc5170c7afb6
kaito <kaitooshiro1@icloud.com>
2020-10-29 03:58:55 +0900

    titleタグ編集

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+$app->useStoragePath(env('APP_STORAGE', base_path() . '/storage'));
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
