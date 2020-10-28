ed2c06ff5f96a9ac0f9e29781525b38985208712
kaito <kaitooshiro1@icloud.com>
2020-10-29 04:19:39 +0900

    metaタグ追加

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="沖縄県出身の奴が沖縄の魅力を伝えるために沖縄クイズを作りました。ITでできることは俺がやります。" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
